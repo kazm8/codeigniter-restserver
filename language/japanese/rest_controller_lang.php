26605a848c84421a003a7ab647ce8762d30fccd4
Kazunori Morikawa <kazm08@gmail.com>
2022-11-28 17:18:08 +0900

    [ADD] Japanese language file

---
+++ language/japanese/rest_controller_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * Japanese language
+ */
+
+$lang['text_rest_invalid_api_key'] = '%s は無効な API キーです'; // %s は REST API キー
+$lang['text_rest_invalid_credentials'] = '認証情報が無効です';
+$lang['text_rest_ip_denied'] = 'IP が拒否されました';
+$lang['text_rest_ip_unauthorized'] = 'IP が認可されませんでした';
+$lang['text_rest_unauthorized'] = '認可されませんでした';
+$lang['text_rest_ajax_only'] = 'AJAXリクエストのみ可能です';
+$lang['text_rest_api_key_unauthorized'] = 'この API キーは要求されたコントローラへのアクセス権を持っていません';
+$lang['text_rest_api_key_permissions'] = 'この API キーは十分な権限を持っていません';
+$lang['text_rest_api_key_time_limit'] = 'この API キーはこのメソッドの回数制限に達しています';
+$lang['text_rest_ip_address_time_limit'] = 'このIPアドレスは、このメソッドの回数制限に達しています';
+$lang['text_rest_unknown_method'] = '不明なメソッド';
+$lang['text_rest_unsupported'] = '未対応のプロトコル';
